0e0307de12f7b8245031c663835fc5f3918f4a62
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-11 15:22:52 -0400

    Landing in Renton 2.0 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -110,7 +110,7 @@
                     this.getWindowWidth();
                 });
                 var_temp_repo = this.findRepoByName('Donation');
-                console.log(temp_repo);
+                console.log(var_temp_repo);
                 if(temp_repo != null && temp_repo != undefined) {
                     this.pageBanner = temp_repo.images[0];
                 } else {
