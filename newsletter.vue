04d1d2988776493d48cfa037ac504954064ef717
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-11 14:56:51 -0400

    Landing in Renton 2.0 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -17,11 +17,11 @@
                                 <div class="row">
                                     <div class="col-sm-6" >
                                         <label for="fieldfihudt" class="accessibility">First Name</label>
-                                        <input v-model="form_data.first_name" required class="margin_20 form-control" id="fieldfihudt" name="cm-f-fihudt" type="text" placeholder="First Name">
+                                        <input v-model="form_data.first_name" required class="margin_20 form-control" id="fieldglrtiu" name="cm-f-glrtiu" type="text" placeholder="First Name">
                                     </div>
                                     <div class="col-sm-6" >
                                         <label for="fieldfihudi" class="accessibility">Last Name</label>
-                                        <input v-model="form_data.last_name" required class="margin_20 form-control" id="fieldfihudi" name="cm-f-fihudi" type="text" placeholder="Last Name">
+                                        <input v-model="form_data.last_name" required class="margin_20 form-control" id="fieldglrtdl" name="cm-f-glrtdl" type="text" placeholder="Last Name">
                                     </div>
                                 </div>
                                 <div class="row">
