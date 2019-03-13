0508f8cdd056b90af4744b9c3ea78aae487d0511
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-13 15:59:27 -0400

    Landing in Renton 2.0 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -189,6 +189,7 @@
                     var subcategories = [];
                     if (dining_cat !== null && dining_cat !== undefined) {
                        subcategories = vm.findSubcategoriesByParentID(dining_cat.id);
+                       console.log(subcategories)
                     }
                     console.log(subcategories)
                     subcategories = _.map(subcategories, 'name').sort();
