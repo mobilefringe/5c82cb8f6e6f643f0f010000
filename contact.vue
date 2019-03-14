2f2932c9fbbc596b30dde6bed187bdddefbd8a21
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-14 12:55:34 -0400

    Landing in Renton 2.0 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -114,11 +114,11 @@
             created() {
                 this.loadData().then(response => {
                     var temp_repo = this.findRepoByName('Contact Us Banner');
-                    if(temp_repo !== null && temp_repo !== undefined) {
-                       temp_repo = temp_repo.images;
-                       this.pageBanner = temp_repo[0];
-                    }
-                    else {
+                    // if(temp_repo !== null && temp_repo !== undefined) {
+                    //   temp_repo = temp_repo.images;
+                    //   this.pageBanner = temp_repo[0];
+                    // }
+                    // else {
                         this.pageBanner = {
                             "image_url": "https://www.mallmaverick.com/system/site_images/photos/000/050/006/original/landing_default_banner.png?1552582181"
                         }
