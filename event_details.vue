bed19beab7f4381d5a64f5f5365b4d0823fef950
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-14 13:05:39 -0400

    Landing in Renton 2.0 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -101,7 +101,7 @@
                             }
                         } else {
                             if (_.includes(this.currentEvent.event_image_url_abs, 'missing')) {
-                                this.currentEvent.image_url = "//codecloud.cdn.speedyrails.net/sites/5b8712636e6f641ebd220000/image/png/1529532187000/eventsplaceholder2@2x.png";    
+                                this.currentEvent.image_url = "//codecloud.cdn.speedyrails.net/sites/5c82cb8f6e6f643f0f010000/image/png/1552583012044/landing_placeholder.png";    
                             }
                         }
 					}
