8281e2c17502320fc6501ca68aa64dd271064112
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-14 12:59:41 -0400

    Landing in Renton 2.0 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -79,7 +79,7 @@
                         this.pageBanner = temp_repo[0];
                     } else {
                         this.pageBanner = {
-                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b5f2c136e6f644fcb5b0100/image/jpeg/1529532304000/insidebanner2.jpg"
+                            "image_url": "https://www.mallmaverick.com/system/site_images/photos/000/050/006/original/landing_default_banner.png?1552582181"
                         }
                     }
                     this.handleButton();
