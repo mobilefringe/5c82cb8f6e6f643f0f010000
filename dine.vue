5919ef32b6c966add216d4f2fa468c98a5ebfa74
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-19 11:57:44 -0400

    Landing in Renton 2.0 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -191,6 +191,7 @@
                     }
                     subcategories = _.map(subcategories, 'name').sort();
                     subcategories.unshift('All Cuisine');
+                    console.log(subcategories)
                     return subcategories;
                 },
                 filterByCategory() {
