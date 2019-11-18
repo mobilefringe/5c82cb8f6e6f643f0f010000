a1ff26c291f87ea8f4b817ee3a455854f62c13b6
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-18 16:23:18 -0500

    Landing in Renton 2.0 1.2  /dine.vue

--- dine.vue
+++ dine.vue
@@ -55,7 +55,9 @@
                             	    <div class="store_logo_container">
                             	        <router-link :to="'/stores/'+ store.slug">
                             	        
-                            	            <div v-if="!store.no_store_logo">
+                            	        
+                            	        
+                            	        <div v-if="!store.no_store_logo">
                                 			    <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="">
                                 			    <img  class="store_img" :src="store.store_front_url_abs" :alt="store.name + 'Logo'">
                                 			</div>
@@ -63,14 +65,11 @@
                                             <div v-else class="no_logo_container">
                                                 <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="">
                                                 <div class="no_logo_text">
-                                                <div class="store_text">
-                                                    <h4>{{ store.name }}</h4>
-                                                    
+                                                    <div class="store_text"><h2>{{ store.name }}</h2></div>
                                                 </div>
                                             </div>
-                                            
-                            	            
-                                			
+                            	        
+                                	
                                 			<div class="store_tag" v-if="store.total_published_promos">
             									<div class="store_tag_text">Promotion</div>
             								</div>
@@ -81,8 +80,10 @@
             									<div class="store_tag_text">New Store</div>
             								</div>
             								<div class="store_details">
-            								    <div class="store_text"><h4>{{ store.name }}</h4></div>    
+            								    <div class="store_text"><h2>{{ store.name }}</h2></div>    
             								</div>
+            								
+                            	        
                                 		</router-link>
                             	    </div>
                                 </div>
