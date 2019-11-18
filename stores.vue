7569fada803ae372a891efad53656bcd91a04b14
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-18 11:33:18 -0500

    Landing in Renton 2.0 1.2  /stores.vue

--- stores.vue
+++ stores.vue
@@ -53,10 +53,9 @@
                                 <div v-masonry-tile  v-for="(store, index) in filteredStores" :key="index" class="stores-grid-item">
                             	    <div class="store_logo_container">
                             	        <router-link :to="'/stores/'+ store.slug">
-                                			<!--<img class="store_img" :src="store.image_url" alt="">-->
-                                			<div v-if="!store.no_store_logo">
+                            	            <div v-if="!store.no_store_logo">
                                 			    <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="">
-                                			    <img  class="store_img" :src="store.store_front_url_abs" :alt="store.name">
+                                			    <img  class="store_img" :src="store.store_front_url_abs" :alt="store.name + 'Logo'">
                                 			</div>
                                 			
                                             <div v-else class="no_logo_container">
@@ -65,6 +64,8 @@
                                                     <div class="store_text"><h4>{{ store.name }}</h4></div>
                                                 </div>
                                             </div>
+                            	        
+                                	
                                 			<div class="store_tag" v-if="store.total_published_promos">
             									<div class="store_tag_text">Promotion</div>
             								</div>
